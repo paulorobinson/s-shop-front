0d847b888d687bd5fb3c6f3c2f3d1112e7a35e7d
Paulo Robinson <paulo.giaciani@gmail.com>
2022-09-23 01:36:11 -0300

    Add and remove Product - Admin Page

--- src/context/Application.js
+++ src/context/Application.js
@@ -1,10 +1,24 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const ApplicationContext = createContext();
 
 const ApplicationProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  const addProduct = (product) => {
+    setProducts([...products, { ...product, id: uuidv4() }]);
+  };
+
+  const removeProduct = (productId) => {
+    const filteredProducts = products.filter(({ id }) => id !== productId);
+    setProducts(filteredProducts);
+  };
+
   return (
-    <ApplicationContext.Provider value={{}}>
+    <ApplicationContext.Provider
+      value={{ addProduct, removeProduct, products }}
+    >
       {children}
     </ApplicationContext.Provider>
   );
--- src/pages/Admin/Products/index.js
+++ src/pages/Admin/Products/index.js
@@ -1,59 +1,76 @@
 import Location from '../../../components/Location';
+import { useApplication } from '../../../context/Application';
 import { Container, ContainerForm, ContainerTable } from './styles';
 
 const Products = () => {
+  const { addProduct, removeProduct, products } = useApplication();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    let dataProduct = {};
+
+    for (let [name, value] of formData) {
+      if (value === '') {
+        alert(`Please enter a value`);
+        return false;
+      }
+      dataProduct = {
+        ...dataProduct,
+        [name]: value,
+      };
+    }
+    addProduct(dataProduct);
+    event.target.reset();
+  };
+
   return (
     <Container>
       <Location location={['Admin', 'Produtos']} />
-      <ContainerForm>
+      <ContainerForm onSubmit={(e) => handleSubmit(e)}>
         <div>
           <p>Produto</p>
-          <input type="text" placeholder="Nome" />
+          <input type="text" name="name" placeholder="Nome" />
         </div>
         <div>
           <p>Descrição</p>
-          <input type="text" placeholder="Descrição" />
+          <input type="text" name="description" placeholder="Descrição" />
         </div>
         <div>
           <p>Preço</p>
-          <input type="text" placeholder="R$ 0,00" />
+          <input type="text" name="price" placeholder="R$ 0,00" />
         </div>
         <div>
           <p>&nbsp;</p>
-          <button type="submit" value="">
-            +
-          </button>
+          <button type="submit">+</button>
         </div>
       </ContainerForm>
       <ContainerTable>
-        <table>
-          <thead>
-            <tr>
-              <th>Produto</th>
-              <th>Descrição</th>
-              <th>Preço</th>
-              <th> </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Produto 001</td>
-              <td>Maria</td>
-              <td>1,99</td>
-              <td>
-                <button>X</button>
-              </td>
-            </tr>
-            <tr>
-              <td>Produto 002</td>
-              <td>Maria</td>
-              <td>1,99</td>
-              <td>
-                <button>X</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        {products.length !== 0 && (
+          <table>
+            <thead>
+              <tr>
+                <th>Produto</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+                <th> </th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product.id}>
+                  <td>{product.name}</td>
+                  <td>{product.description}</td>
+                  <td>{product.price}</td>
+                  <td>
+                    <button onClick={() => removeProduct(product.id)}>X</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </ContainerTable>
     </Container>
   );
